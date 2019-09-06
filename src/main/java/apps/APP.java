8d40a7b6a243ec070530b6af8a992d412e2e36b0
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2019-09-06 00:04:53 -0500

    errores

--- src/main/java/apps/APP.java
+++ src/main/java/apps/APP.java
@@ -1,41 +1,41 @@
 package apps;
 
-import anotaciones.aWeb;;
+import anotaciones.AWeb;;
 
 public class APP {
 
 
 
-    @aWeb(value = "prueba")
+    @AWeb(value = "prueba")
     public static String pruebaWeb (){
         return "prueba";
     }
 
-    @aWeb("suma")
+    @AWeb("suma")
 	public static String suma(String num1, String num2) {
         //System.out.println(num1 +""+num2);
 		return "La suma de "+ num1 +"+"+ num2 +"=" + Integer.toString(Integer.parseInt(num1)+Integer.parseInt(num2));
     }
 
-    @aWeb("resta")
+    @AWeb("resta")
 	public static String resta(String num1, String num2) {
    
 		return "La suma de "+ num1 +"-"+ num2 +"=" + Integer.toString(Integer.parseInt(num1)-Integer.parseInt(num2));
     }
     
-    @aWeb("multi")
+    @AWeb("multi")
 	public static String multi(String num1, String num2) {
         //System.out.println(num1 +""+num2);
 		return "La multiplicacion de "+ num1 +"*"+ num2 +"=" + Integer.toString(Integer.parseInt(num1)*Integer.parseInt(num2));
     }
     
-    @aWeb("div")
+    @AWeb("div")
 	public static String division(String num1, String num2) {
         //System.out.println(num1 +""+num2);
 		return "La division de "+ num1 +"/"+ num2 +"=" + Float.toString(Integer.parseInt(num1)/Integer.parseInt(num2));
     }
     
-    @aWeb("potencia")
+    @AWeb("potencia")
 	public static String potencia(String num1,String num2) {
         //System.out.println(num1 +""+num2);
 		return "el numero  "+ num1 +"al" + num2 +"es =" + Integer.toString((int ) Math.pow(Integer.parseInt(num1), Integer.parseInt(num2)));
--- src/main/java/edu/eci/arep/proyecto/AppServer.java
+++ src/main/java/edu/eci/arep/proyecto/AppServer.java
@@ -18,7 +18,7 @@ import javax.imageio.ImageIO;
 import javax.sound.midi.Patch;
 
 
-import anotaciones.aWeb;
+import anotaciones.AWeb;
 
 
 
@@ -34,7 +34,7 @@ public class AppServer {
             for( Method m : c.getDeclaredMethods()){
                 if (m.getAnnotations().length > 0){
                     Handler handler = new StaticMethodHandler(m);
-                    load("/apps/"+m.getDeclaredAnnotation(aWeb.class).value(),handler);
+                    load("/apps/"+m.getDeclaredAnnotation(AWeb.class).value(),handler);
                 }
                 
             }
