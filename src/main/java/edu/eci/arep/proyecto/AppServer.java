60d04043702dd056a2a32767f7afdc7161f33d64
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2019-09-22 19:37:18 -0500

    prueba 2

--- src/main/java/edu/eci/arep/proyecto/AppServer.java
+++ src/main/java/edu/eci/arep/proyecto/AppServer.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import static edu.eci.arep.proyecto.AppServer.controlReq;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,123 +17,116 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
 
 import anotaciones.Aweb;
-import edu.eci.arep.proyecto.socket.SerSocket;
 
 /**
- * Clase principal en donde estan dos metodos fundamentales el incializar y el
- * escuchar
+ * Clase principal en donde estan dos metodos fundamentales el incializar y el escuchar
  */
 
-public class AppServer implements Runnable {
+public class AppServer {
 
-    private static HashMap<String, Handler> listaUrl = new HashMap<String, Handler>();
-    private Socket cliente;
-    private static final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
-    private ServerSocket serverSocket;
+    private static HashMap<String,Handler> listaUrl = new HashMap<String,Handler>();
 
-    public AppServer(Socket cliente) {
-        this.cliente = cliente;
-    }
-
-    @Override
-    public void run() {
-        String path = controlReq(cliente);
-        requests(cliente, path);
-        try {
-            cliente.close();
-            //serverSocket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(AppServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public static String controlReq(Socket cliente) {
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-        } catch (IOException ex) {
-            Logger.getLogger(AppServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        String inputLine;
-        String request = "";
-        try {
-            while ((inputLine = in.readLine()) != null) {
-                if (inputLine.matches("(GET)+.*"))
-                    request = inputLine.split(" ")[1];
-                if (!in.ready())
-                    break;
-            }
-            if (request == null) {
-                request = "/error.html";
-            } else if (request.equals("/")) {
-                request = "/index.html";
-
-            }
-            System.out.println(request);
-        } catch (IOException ex) {
-            Logger.getLogger(AppServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return request;
-
-    }
-
+    public static int PORT = 4567;
     /**
-     * Este metodo nos permite ver todos lo metodos que tiene la clase APP y pueden
-     * ejecutarse dichos metodos con respecto a una clase creada por el
-     * desarrollador
+     * Este metodo nos permite ver todos lo metodos que tiene la clase APP y pueden ejecutarse dichos metodos con respecto a una clase creada por el desarrollador
      */
     public static void inicializar() {
         try {
             File f = new File(System.getProperty("user.dir") + "/src/main/java/apps");
             File[] ficheros = f.listFiles();
-            // Reflections reflections = new Reflections("apps", new
-            // SubTypesScanner(false));
-            // Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
-            for (File fs : ficheros) {
+            //Reflections reflections = new Reflections("apps", new SubTypesScanner(false));
+            //Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+            for (File fs :ficheros){
                 String name = fs.getName();
-                name = "apps." + name.substring(0, name.indexOf("."));
+                name = "apps." + name.substring(0,name.indexOf("."));
                 Class<?> c = Class.forName(name);
                 for (Method m : c.getMethods()) {
-                    if (m.getAnnotations().length > 0) {
+                    if (m.getAnnotations().length > 0){
                         Handler handler = new StaticMethodHandler(m);
-                        load("/apps/" + m.getDeclaredAnnotation(Aweb.class).value(), handler);
+                        load("/apps/"+m.getDeclaredAnnotation(Aweb.class).value(),handler);
                     }
-
+                    
                 }
             }
-
-            // System.out.println(c.getDeclaredAnnotations().length);
+            
+            //System.out.println(c.getDeclaredAnnotations().length);
         } catch (Exception e) {
-
+            
             e.printStackTrace();
         }
     }
+    /**
+     * El metodo listen es el encargado de tener el servidor web funcionado y es el que escucha cualqueir peticion y a cada instante y retorna cada peticion con su respectivo 
+     * recurso, si el servidor conoce la direccion 
+     * @throws IOException
+     */
+    public static void listen() throws IOException {
 
-    private  void requests(Socket clientSocket, String request) {
-        PrintWriter out = null;
+        ServerSocket serverSocket = null;
         try {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-        } catch (IOException ex) {
-            Logger.getLogger(AppServer.class.getName()).log(Level.SEVERE, null, ex);
+            serverSocket = new ServerSocket(getPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 4567.");
+            System.exit(1);
         }
-        if (request.matches("(/apps/).*")) {
-            // System.out.println(request);
+        while (true) {
+            Socket clientSocket = null;
+            try {
+                System.out.println("Ready to listen ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String request = "";
+            String inputLine;
+
+            while ((inputLine = in.readLine())!= null) {
+                if (inputLine.matches("(GET)+.*"))
+                    request = inputLine.split(" ")[1];
+                if (!in.ready())
+                    break;
+            }
+            //System.out.println(request);
+            if (request == null){
+                request = "/error.html";
+            }else if (request.equals("/")){
+                request = "/index.html";
+    
+            }
+            requests(request, out, clientSocket.getOutputStream());
+            
+
+            out.close();
+            in.close();
+            clientSocket.close();
+            serverSocket.close();
+
+        }
+
+    }
+
+    private static void requests(String request, PrintWriter out, OutputStream outputStream) throws IOException {
+        if (request.matches("(/apps/).*")){
+            //System.out.println(request);
             Object[] parametros = extParams(request);
-            request = request.subSequence(0, request.indexOf("?")).toString();
-            // System.out.println(listaUrl.values().toString());
+            request = request.subSequence(0,request.indexOf("?")).toString();
+            //System.out.println(listaUrl.values().toString());
             if (listaUrl.containsKey(request)) {
                 out.print("HTTP/1.1 200 OK \r");
                 out.print("Content-Type: text/html \r\n");
                 out.print("\r\n");
                 try {
-
-                    out.println(parametros == null ? listaUrl.get(request).procesar()
-                            : listaUrl.get(request).procesar(parametros));
-
+                    
+                    out.println(parametros == null ? listaUrl.get(request).procesar() : listaUrl.get(request).procesar(parametros));
+                
                 } catch (IllegalAccessException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -146,11 +138,11 @@ public class AppServer implements Runnable {
                     e.printStackTrace();
                 }
             }
-        } else {
+        }else{
             if (request.endsWith(".png")) {
-                readImage(out, clientSocket, request);
+                readImage(out,outputStream,request);
 
-            } else if (request.endsWith("/linkin")) {
+            }else if (request.endsWith("/linkin")) {
                 linkin(out);
 
             } else if (request.endsWith(".html")) {
@@ -161,52 +153,46 @@ public class AppServer implements Runnable {
 
         }
 
-    }
-
-    private  void readImage(PrintWriter out, Socket cliente, String request) {
-        File graphicResource = new File("resource/" + request);
-
-        try {
-            FileInputStream inputImage = new FileInputStream(graphicResource);
-            byte[] bytes = new byte[(int) graphicResource.length()];
-            inputImage.read(bytes);
-
-            DataOutputStream binaryOut;
-            binaryOut = new DataOutputStream(cliente.getOutputStream());
-            binaryOut.writeBytes("HTTP/1.1 200 OK \r\n");
-            binaryOut.writeBytes("Content-Type: image/png\r\n");
-            binaryOut.writeBytes("Content-Length: " + bytes.length);
-            binaryOut.writeBytes("\r\n\r\n");
-            binaryOut.write(bytes);
-            binaryOut.close();
-        } catch (IOException e) {
-            
-            e.printStackTrace();
-        }
-
-    }
-
-    private static void readHTML(PrintWriter out, String request) {
-        BufferedReader bf;
-        try {
-            bf = new BufferedReader(new FileReader("resource" + request));
-            out.print("HTTP/1.1 200 OK \r");
-            out.print("Content-Type: text/html \r\n");
-            out.print("\r\n");
-            String line;
-            while ((line = bf.readLine()) != null) {
-                out.print(line);
-            }
-        } catch (IOException e) {
-           
-            e.printStackTrace();
-        }
         
     }
+    
+
+    private static void readImage(PrintWriter out, OutputStream outStream, String request) throws IOException {
+        File graphicResource= new File("resource/" +request);
+        FileInputStream inputImage = new FileInputStream(graphicResource);
+        byte[] bytes = new byte[(int) graphicResource.length()];
+        inputImage.read(bytes);
+
+        DataOutputStream binaryOut;
+        binaryOut = new DataOutputStream(outStream);
+        binaryOut.writeBytes("HTTP/1.1 200 OK \r\n");
+        binaryOut.writeBytes("Content-Type: image/png\r\n");
+        binaryOut.writeBytes("Content-Length: " + bytes.length);
+        binaryOut.writeBytes("\r\n\r\n");
+        binaryOut.write(bytes);
+        binaryOut.close();
+    }
+
+    private static void readHTML(PrintWriter out, String request) throws IOException {
+        BufferedReader bf = new BufferedReader(new FileReader( "resource" + request ));
+        out.print("HTTP/1.1 200 OK \r");
+        out.print("Content-Type: text/html \r\n");
+        out.print("\r\n");
+        String line;
+        while ((line = bf.readLine()) != null) {
+            out.print(line);
+        }
+    }
 
 
 
-   
+    private static int getPort() {
+
+		if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567; //returns default port if heroku-port isn't set (i.e.on localhost)
+    }
 	
 
 
